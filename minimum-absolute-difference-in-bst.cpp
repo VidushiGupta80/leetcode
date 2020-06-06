c43afd14077cee2b3de9aee9471971ba1b2389bf
VidushiGupta80 <guptavidushi80.vg@gmail.com>
2020-06-06 05:30:00 +0530

    Adding solution to Minimum Absolute Difference in BST

---
+++ minimum-absolute-difference-in-bst.cpp
@@ -0,0 +1,50 @@
+//Minimum Absolute Difference in BST -> 06/06/2020 19:10
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private:
+    bool isLeaf(TreeNode* root)
+    {
+        if(root->left == NULL && root->right == NULL)
+            return true;
+        return false;
+    }
+    
+    void inOrder(TreeNode* root, vector<int>& ans)
+    {
+        if(isLeaf(root))
+        {
+            ans.push_back(root->val);
+            return;
+        }
+        
+        if(root->left)
+            inOrder(root->left, ans);
+        ans.push_back(root->val);
+        if(root->right)
+            inOrder(root->right, ans);
+        
+        return;
+    }
+public:
+    int getMinimumDifference(TreeNode* root) {
+        vector<int> ans;
+        inOrder(root, ans);
+        int minimum = INT_MAX;
+        int len = ans.size();
+        for(int i = 0, j = i + 1; j < len; i++, j++)
+        {
+            minimum = min(minimum, abs(ans[i] - ans[j]));
+        }
+        return minimum;
+    }
+};
