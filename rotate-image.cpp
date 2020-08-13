363b591920736b96c269b948561ad8db92ed81a5
VidushiGupta80 <guptavidushi80.vg@gmail.com>
2020-08-13 05:30:00 +0530

    Adding solution to Rotate Image

---
+++ rotate-image.cpp
@@ -0,0 +1,18 @@
+//Rotate Image -> 08/13/2020 14:07
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        for(int i = 0; i < matrix.size(); i++)
+        {
+            for(int j = i; j < matrix.size(); j++)
+            {
+                swap(matrix[i][j], matrix[j][i]);
+            }
+        }
+        for(int i = 0; i < matrix.size(); i++)
+        {
+            reverse(matrix[i].begin(), matrix[i].end());
+        }
+        return;
+    }
+};
