e5473fdde74a70353447ccbcb47340b892750543
VidushiGupta80 <guptavidushi80.vg@gmail.com>
2020-08-16 05:30:00 +0530

    Adding solution to Best Time to Buy and Sell Stock

---
+++ best-time-to-buy-and-sell-stock-iii.cpp
@@ -0,0 +1,16 @@
+//Best Time to Buy and Sell Stock -> 08/16/2020 19:15
+class Solution {
+
+public:
+    int maxProfit(vector<int>& prices) {
+        int firstBuy = INT_MAX, secondBuy = INT_MAX, firstProfit = 0, secondProfit = 0, len = prices.size();
+        for(int currPrice: prices) {
+            firstBuy = min(firstBuy, currPrice);
+            firstProfit = max(firstProfit, currPrice - firstBuy);
+            secondBuy = min(secondBuy, currPrice - firstProfit);
+            secondProfit = max(secondProfit, currPrice - secondBuy);
+        }
+        return secondProfit;
+        
+    }
+};
