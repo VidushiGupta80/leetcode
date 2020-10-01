ae2d017cec7879a428dee7c24508cdeec5ed0655
VidushiGupta80 <guptavidushi80.vg@gmail.com>
2020-10-01 05:30:00 +0530

    Adding solution to Single Number

---
+++ single-number.cpp
@@ -0,0 +1,10 @@
+//Single Number -> 10/01/2020 13:53
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int ans = 0;
+        for(int i = 0; i < nums.size(); i++)
+            ans = ans ^ nums[i];
+        return ans;
+    }
+};
